8e7b9a4f8b96bb864c292a270b15163e222bf94b
JUanM776 <Juan.cordobaflore@campusucc.edu.co>
2026-04-23 11:05:15 -0500

    Crear autor

---
+++ proyecto/gestion/forms.py
@@ -0,0 +1,9 @@
+class AutorForm(forms.ModelForm): 
+    class Meta: 
+        model = Autor 
+        fields = ['nombre', 'correo', 'nacionalidad', 'fecha_nacimiento', 'biografia'] 
+ 
+class LibroForm(forms.ModelForm): 
+    class Meta: 
+        model = Libro 
+        fields = ['titulo', 'fecha_publicacion', 'genero', 'isbn', 'autor']
