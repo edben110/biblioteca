00e00e7b1d9322fbff2143627dda1479c60cf9a4
edben110 <edben110@gmail.com>
2026-04-23 11:48:40 -0500

    new eliminar function

--- proyecto/gestion/views.py
+++ proyecto/gestion/views.py
@@ -1,67 +1,7 @@
-from django.shortcuts import render, redirect, get_object_or_404
 from django.views.generic import ListView, CreateView, UpdateView, DeleteView
 from django.urls import reverse_lazy
 from .models import Autor, Libro
 from .forms import AutorForm, LibroForm
-
-# =============================================
-# CRUD Autores
-# =============================================
-
-def lista_autores(request):
-    autores = Autor.objects.all()
-    return render(request, 'gestion/lista_autores.html', {'autores': autores})
-
-def crear_autor(request):
-    if request.method == 'POST':
-        form = AutorForm(request.POST)
-        if form.is_valid():
-            form.save()
-            return redirect('lista_autores')
-    else:
-        form = AutorForm()
-    return render(request, 'gestion/crear_autor.html', {'form': form})
-
-def editar_autor(request, pk):
-    autor = get_object_or_404(Autor, pk=pk)
-    if request.method == 'POST':
-        form = AutorForm(request.POST, instance=autor)
-        if form.is_valid():
-            form.save()
-            return redirect('lista_autores')
-    else:
-        form = AutorForm(instance=autor)
-    return render(request, 'gestion/editar_autor.html', {'form': form})
-
-def eliminar_autor(request, pk):
-    autor = get_object_or_404(Autor, pk=pk)
-    if request.method == 'POST':
-        autor.delete()
-        return redirect('lista_autores')
-    return render(request, 'gestion/autor_confirm_delete.html', {'autor': autor})
-# =============================================
-# CRUD Libros
-# =============================================
-
-# Vista normal (función) - Listar libros
-def lista_libros(request):
-    libros = Libro.objects.select_related('autor').all()
-    return render(request, 'list_view.html', {'libros': libros})
-
-# Vista genérica (clase) - Crear libro
-class CrearLibroView(CreateView):
-    model = Libro
-    form_class = LibroForm
-    template_name = 'create_view.html'
-    success_url = reverse_lazy('lista_libros')
-
-# Vista genérica (clase) - Editar libro
-class EditarLibroView(UpdateView):
-    model = Libro
-    form_class = LibroForm
-    template_name = 'edit_view.html'
-    success_url = reverse_lazy('lista_libros')
-
 
 # =============================================
 # CRUD Generico (Class-Based Views)
