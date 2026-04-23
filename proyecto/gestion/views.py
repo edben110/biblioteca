62742d807b70759e2187073ab6d14f6f84d38eeb
edben110 <edben110@gmail.com>
2026-04-23 11:37:41 -0500

    new eliminar function

--- proyecto/gestion/views.py
+++ proyecto/gestion/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect, get_object_or_404
-from django.views.generic import CreateView, UpdateView
+from django.views.generic import ListView, CreateView, UpdateView, DeleteView
 from django.urls import reverse_lazy
 from .models import Autor, Libro
 from .forms import AutorForm, LibroForm
@@ -33,6 +33,12 @@ def editar_autor(request, pk):
         form = AutorForm(instance=autor)
     return render(request, 'gestion/editar_autor.html', {'form': form})
 
+def eliminar_autor(request, pk):
+    autor = get_object_or_404(Autor, pk=pk)
+    if request.method == 'POST':
+        autor.delete()
+        return redirect('lista_autores')
+    return render(request, 'gestion/autor_confirm_delete.html', {'autor': autor})
 # =============================================
 # CRUD Libros
 # =============================================
@@ -55,3 +61,66 @@ class EditarLibroView(UpdateView):
     form_class = LibroForm
     template_name = 'edit_view.html'
     success_url = reverse_lazy('lista_libros')
+
+
+# =============================================
+# CRUD Generico (Class-Based Views)
+# =============================================
+
+# Autor - vistas genericas
+class ListaAutorGenericaView(ListView):
+    model = Autor
+    template_name = 'genericas/autores/lista_autor_generica.html'
+    context_object_name = 'autores'
+
+
+class CrearAutorGenericaView(CreateView):
+    model = Autor
+    form_class = AutorForm
+    template_name = 'genericas/autores/crear_autor_generica.html'
+    success_url = reverse_lazy('lista_autores_generica')
+
+
+class EditarAutorGenericaView(UpdateView):
+    model = Autor
+    form_class = AutorForm
+    template_name = 'genericas/autores/editar_autor_generica.html'
+    success_url = reverse_lazy('lista_autores_generica')
+
+
+class EliminarAutorGenericaView(DeleteView):
+    model = Autor
+    template_name = 'genericas/autores/eliminar_autor_generica.html'
+    success_url = reverse_lazy('lista_autores_generica')
+    context_object_name = 'autor'
+
+
+# Libro - vistas genericas
+class ListaLibroGenericaView(ListView):
+    model = Libro
+    template_name = 'genericas/libros/lista_libro_generica.html'
+    context_object_name = 'libros'
+
+    def get_queryset(self):
+        return Libro.objects.select_related('autor').all()
+
+
+class CrearLibroGenericaView(CreateView):
+    model = Libro
+    form_class = LibroForm
+    template_name = 'genericas/libros/crear_libro_generica.html'
+    success_url = reverse_lazy('lista_libros_generica')
+
+
+class EditarLibroGenericaView(UpdateView):
+    model = Libro
+    form_class = LibroForm
+    template_name = 'genericas/libros/editar_libro_generica.html'
+    success_url = reverse_lazy('lista_libros_generica')
+
+
+class EliminarLibroGenericaView(DeleteView):
+    model = Libro
+    template_name = 'genericas/libros/eliminar_libro_generica.html'
+    success_url = reverse_lazy('lista_libros_generica')
+    context_object_name = 'libro'
